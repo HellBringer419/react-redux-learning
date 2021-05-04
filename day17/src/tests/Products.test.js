e639188ca2e1ee070412569dcaa3c3dfe6094924
HellBringer419 <hell_bringer419@outlook.com>
2021-05-04 10:45:27 +0530

    added some pagination tests

--- day17/src/tests/Products.test.js
+++ day17/src/tests/Products.test.js
@@ -2,15 +2,25 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Products, { PRODUCTS_PER_PAGE } from "../components/Products";
 
-// beforeEach( () => {});
-
-test(`should contain ${PRODUCTS_PER_PAGE} product(s) on initial page`, async () => {
+beforeEach(() => {
 	render(
 		<Router>
 			<Products />
 		</Router>
 	);
-	const productCardElements = await screen.findAllByRole("gridcell");
+});
 
+test(`should contain ${PRODUCTS_PER_PAGE} product(s) on initial page`, async () => {
+	const productCardElements = await screen.findAllByRole("gridcell");
 	expect(productCardElements).toHaveLength(PRODUCTS_PER_PAGE);
+});
+
+test("should have pagination on the document", async () => {
+	const paginationContainer = await screen.findByRole("tablist");
+	expect(paginationContainer).toBeInTheDocument();
+});
+
+test(`should have ${2 / PRODUCTS_PER_PAGE} pages to paginate`, async () => {
+	const paginationButtons = await screen.findAllByRole("tab");
+	expect(paginationButtons).toHaveLength(2 / PRODUCTS_PER_PAGE);
 });
